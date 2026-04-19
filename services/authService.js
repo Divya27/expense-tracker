f3ab6bf1a51279659a47489e48ae7b3d89409e14
Divya Ahuja <divyaahuja@Divyas-MacBook-Pro.local>
2026-04-19 16:09:07 +0530

    feat: added created date in response of login endpoint

--- services/authService.js
+++ services/authService.js
@@ -48,7 +48,8 @@ const login = async ({ email, password }) => {
       id: user.id,
       first_name: user.first_name,
       last_name: user.last_name,
-      email: user.email
+      email: user.email,
+      created_at: user.created_at
     }
   };
 };
